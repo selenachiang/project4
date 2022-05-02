fd07d64113e13e37e3855f9fc75a9b37f7aebc4c
cc83 <cc83@cs-ssh.argo.uwf.edu>
2022-05-01 23:41:04 -0500

    iii

--- text-menu-app.cpp
+++ text-menu-app.cpp
@@ -13,15 +13,38 @@ TextMenuApp::TextMenuApp() {
 void TextMenuApp::run() {
 	
 	vector<string> messages = {
+		// 0
 		"Input the file name to load initial data from: ",
+		// 1
 		"File cannot be opened! Input again!\n",
+		// 2
 		"1. Print all patients;\n",
+		// 3
 		"2. Update information of a patient;\n",
-		"2. Update information of a patient;\n",
+		// 4
+		"3. Save file;\n",
+		// 5
 		"4. Exit.\n",
+		// 6
 		"Please choose an option(1-4): ",
+		// 7
 		"The choice should be between 1 and 4!\n",
-		" patients in total.\n \n"
+		// 8
+		" patients in total.\n \n",
+		// 9
+		"Please input patient's id as shown in the list: ",
+		// 10
+		"Invalid index, please input again!\n",
+		// 11
+		"The existing info:\n",
+		// 12
+		"Please input patient's new name: ",
+		// 13
+		"Please input patient's new age: ",
+		// 14
+		"Please input patients's new gender(Male/Female): ",
+		// 15
+		"Patient updated!\n"
 	};
 
 	// OPEN INPUT FILE TO INITIALIZE PATIENTS
@@ -51,12 +74,31 @@ void TextMenuApp::run() {
 			cout << messages[7];
 		}
 		switch (userInput) {
-			case 1:
+			case 1: {
 				this->patients.printAll();
 				cout << this->patients.getCount() << messages[8];
 				break;
-			default:
+			}
+			case 2: {
+				int index = 0;
+				string name;
+				int age;
+				string gender;
+				cout << messages[9] << endl;
+				cin >> index;
+				cout << messages[12];
+				cin >> name;
+				cout << messages[13];
+				cin >> age;
+				cout << messages[14];
+				cin >> gender;
+				this->patients.updatePatient(index, name, age, gender);
+				cout << messages[15];
 				break;
+			}
+			default: {
+				break;
+			}
 		}
 	}
 	while (userInput != 4);
